73e73c16b832e793d1a771fb66fb35555eab4749
vrindgupta <vrind.gupta1@gmail.com>
2023-05-16 20:51:07 -0400

    Added Caching values and cache evict and cache put

--- src/main/java/com/vrind/employee/rest/EmployeeRestController.java
+++ src/main/java/com/vrind/employee/rest/EmployeeRestController.java
@@ -3,6 +3,8 @@ package com.vrind.employee.rest;
 import com.vrind.employee.dto.EmployeeDTO;
 import com.vrind.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +23,12 @@ public class EmployeeRestController {
     }
 
     @GetMapping("/")
-    @Cacheable(value="employees")
     public ResponseEntity<List<EmployeeDTO>> getAllEmployees(){
         return ResponseEntity.ok().body(employeeService.findAll());
     }
 
     @GetMapping("/{empId}")
+    @Cacheable(value = "employees" , key = "'Employee' + #empId")
     public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable int empId){
         return ResponseEntity.ok().body(employeeService.findById(empId));
     }
@@ -37,11 +39,13 @@ public class EmployeeRestController {
     }
 
     @PutMapping("/")
+    @CachePut(value = "employees", key = "'Employee' + #employeeDTO.getId()")
     public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody EmployeeDTO employeeDTO){
         return ResponseEntity.ok().body(employeeService.updateEmployee(employeeDTO));
     }
 
     @DeleteMapping("/{empId}")
+    @CacheEvict(value = "employees", key = "'Employee' + #employeeDTO.getId()")
     public ResponseEntity<EmployeeDTO> deleteEmployee(@PathVariable int empId){
         return ResponseEntity.ok().body(employeeService.deleteEmployee(empId));
     }
